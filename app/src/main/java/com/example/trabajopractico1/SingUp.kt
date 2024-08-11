225d77add9832e18139a60501ae55946424ae89f
Maximo <maxipreneste@gmail.com>
2024-08-11 20:28:34 -0300

    fixes en registro, WTF btn

--- app/src/main/java/com/example/trabajopractico1/SingUp.kt
+++ app/src/main/java/com/example/trabajopractico1/SingUp.kt
@@ -11,37 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.trabajopractico1.ui.theme.TrabajoPractico1Theme
 
 class SingUp : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            TrabajoPractico1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.sing_up)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrabajoPractico1Theme {
-        Greeting("Android")
-    }
-}
