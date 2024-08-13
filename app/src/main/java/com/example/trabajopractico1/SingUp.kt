600cd568625b37ba89856e58bc760a6557959b18
Maximo <maxipreneste@gmail.com>
2024-08-13 19:35:54 -0300

    le agrego logica al la activity de registro, siguen mal los botones

--- app/src/main/java/com/example/trabajopractico1/SingUp.kt
+++ app/src/main/java/com/example/trabajopractico1/SingUp.kt
@@ -1,25 +1,57 @@
 package com.example.trabajopractico1
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.trabajopractico1.ui.theme.TrabajoPractico1Theme
 
 class SingUp : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.sing_up)
+        val acept=findViewById<Button>(R.id.acept)
+        acept.setOnClickListener(){
+            if (verificationPass()&&validateUsername()&&validateMail()){
+                startActivity(Intent(this,MainActivity::class.java))
+            }
+
+        }
+    }
+    private fun verificationPass():Boolean{
+        val pass=findViewById<EditText>(R.id.pass)
+        val veriPass=findViewById<EditText>(R.id.veri)
+        if (pass.text.toString()==veriPass.text.toString()&&validatePass(pass.text.toString())){
+            return true
+        }
+        return false
+    }
+    private fun validatePass(pass:String):Boolean{
+        if (pass.length>=6){ //lo hago funcion porque posteriormente puedo validadr con expresiones regulares (el enunciado pide min 6 caracteres, pero puedo avanzar)
+            return true
+        }
+        return false
+    }
+    private fun validateUsername():Boolean{
+        val user=findViewById<EditText>(R.id.user)
+        if (user.text.toString().isNotBlank()){
+            Log.i("user","ok")
+            return true
+        }
+        Log.i("user","no")
+        return false
+    }
+    private fun validateMail():Boolean{
+        val mail=findViewById<EditText>(R.id.mail)
+        if (mail.text.toString().isNotBlank()){
+            Log.i("mail","ok")
+            return true
+        }
+        Log.i("mail","no")
+        return false
     }
 }
 
