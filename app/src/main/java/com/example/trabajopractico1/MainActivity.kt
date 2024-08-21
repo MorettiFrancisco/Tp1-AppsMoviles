036b6d32766a1042d3518c19b6c272bbd8921c4a
MorettiFrancisco <fmoretti@alu.frlp.utn.edu.ar>
2024-08-20 21:28:38 -0300

    Button Iniciar Sesion logic set

--- app/src/main/java/com/example/trabajopractico1/MainActivity.kt
+++ app/src/main/java/com/example/trabajopractico1/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.trabajopractico1
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,17 +12,41 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         val btnSingUp=findViewById<Button>(R.id.btnRegister)
-        btnSingUp.setOnClickListener(){
-            val intent=Intent(this,SingUp::class.java)
+        btnSingUp.setOnClickListener {
+            val intent= Intent(this,SingUp::class.java)
             startActivity(intent)
+        }
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val editTextUser = findViewById<EditText>(R.id.etUser)
+        val editTextPassword = findViewById<EditText>(R.id.etPassword)
+        val errorTextView = findViewById<TextView>(R.id.errorTextView) // Agrega un TextView en tu XML para mostrar errores
+
+        btnLogin.setOnClickListener {
+            val username = editTextUser.text.toString()
+            val password = editTextPassword.text.toString()
+
+            // Datos hardcodeados para la verificación
+            val validUsername = "usuario"
+            val validPassword = "123456"
+
+            if (username == validUsername && password == validPassword) {
+                // Si coinciden, procede con la acción (por ejemplo, navegar a otra actividad)
+                val intent = Intent(this, Login::class.java)
+                startActivity(intent)
+            } else {
+                // Si no coinciden, muestra un mensaje de error
+                errorTextView.text = "Usuario y/o contraseña incorrectos"
+                errorTextView.visibility = TextView.VISIBLE
+            }
         }
     }
 }
